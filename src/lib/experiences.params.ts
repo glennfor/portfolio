9b7b8559217d9b289c14566aac88ee12ac396dc8
Glen Nfor <glennfor72@gmail.com>
2023-12-25 22:24:29 -0500

    chore: add more experience items

--- src/lib/experiences.params.ts
+++ src/lib/experiences.params.ts
@@ -4,20 +4,40 @@ import { ContractType, type Experience } from './types';
 
 const MY_EXPERIENCES: Array<Experience> = [
 	{
-		slug: 'open-sourcer',
-		company: 'Self-employed',
+		slug: 'vtc',
+		company: 'Visionary Techonoligies Company',
+		description: 'Creating local software solutions.',
+		contract: ContractType.Internship,
+		type: 'Software Development',
+		location: 'Yaounde, Cameroon',
+		period: { from: new Date(2023, 4, 22) , to: new Date(2023, 10, 30)},
+		skills: getSkills('django', 'js', 'html', 'tw', 'css', 'jquery'),
+		name: 'Full Stack Developer',
+		color: '#f50',
+		links: [{
+			to:"https://vtcinternationale.pythonanywhere.com/", label:"Attendance Management System Prototype", newTab: true
+		}],
+		logo: Assets.VTC,
+		tasks: [
+			"Create ERP software for local companies"
+		]
+	},
+
+	{
+		slug: 'simba',
+		company: 'Simba',
 		description: 'Creating awesome tools for developers.',
 		contract: ContractType.SelfEmployed,
 		type: 'Software Development',
-		location: 'Home',
-		period: { from: new Date() },
+		location: 'Yaounde, Cameroon',
+		period: { from: new Date(2022, 0), to: new Date(2023, 11) },
 		skills: getSkills('ts', 'js'),
-		name: 'Open Source Developer',
+		name: 'Junior Developer',
 		color: '#ffffff',
 		links: [],
 		logo: Assets.Unknown,
-		shortDescription: ''
 	},
+
 	{
 		slug: 'software-freelance',
 		company: 'Self-employed',
@@ -25,29 +45,18 @@ const MY_EXPERIENCES: Array<Experience> = [
 		contract: ContractType.Freelance,
 		type: 'Software Development',
 		location: 'Home',
-		period: { from: new Date() },
-		skills: getSkills('svelte', 'ts', 'sass', 'css', 'html', 'js'),
+		period: { from: new Date(2022, 0) },
+		skills: getSkills('jquery', 'css', 'html', 'js'),
 		name: 'Freelancer',
 		color: '#ffffff',
-		links: [],
+		links: [{
+			to: "https://ket-academy.github.io/",
+			label: "Older KET Academy Website",
+			newTab: true,
+		}],
 		logo: Assets.Unknown,
-		shortDescription: ''
 	},
-	{
-		slug: 'software-freelance-junior',
-		company: 'Self-employed',
-		description: 'Creating awesome applications for customers.',
-		contract: ContractType.Freelance,
-		type: 'Software Development',
-		location: 'Home',
-		period: { from: new Date(2022, 0, 1), to: new Date() },
-		skills: getSkills('css', 'html', 'js'),
-		name: 'Junior Freelancer',
-		color: '#ffffff',
-		links: [],
-		logo: Assets.Unknown,
-		shortDescription: ''
-	}
+	
 ];
 
 export default MY_EXPERIENCES;
