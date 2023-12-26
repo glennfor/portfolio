d13ab6af9cafbb294363600d26645a20a240a6ae
Glen Nfor <glennfor72@gmail.com>
2023-12-25 22:27:16 -0500

    chore: add more project items

--- src/lib/projects.params.ts
+++ src/lib/projects.params.ts
@@ -4,36 +4,35 @@ import type { Project } from './types';
 
 const MY_PROJECTS: Array<Project> = [
 	{
-		slug: 'slick-portfolio-angular',
+		slug: 'ket-academy',
 		color: '#5e95e3',
 		description:
-			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore non dolores voluptatibus vitae praesentium aperiam, iure laboriosam repellendus sunt explicabo pariatur totam enim, nihil animi quisquam. Sit vero quod laborum!',
-		shortDescription:
-			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore non dolores voluptatibus vitae praesentium aperiam, iure laboriosam repellendus sunt explicabo pariatur totam enim, nihil animi quisquam. Sit vero quod laborum!',
-		links: [{ to: 'https://github.com/RiadhAdrani/slick-portfolio-svelte', label: 'GitHub' }],
+			'Hybrid online learning platform for KET academy.',
+		shortDescription:'Hybrid online learning platform for KET academy.',
+		links: [{ to: 'https://ketacademy.web.app', label: 'Visit Website' , newTab: true}],
 		logo: Assets.KetAcademy,
-		name: 'Slick Portfolio',
+		name: 'KET Academy',
 		period: {
-			from: new Date()
+			from: new Date(2023, 4), 
 		},
-		skills: getSkills('angular', 'ts', 'tailwind'),
-		type: 'Website Template'
+		skills: getSkills('reactjs', 'tailwind', 'scss', 'firebase'),
+		type: 'Web application'
 	},
 	{
-		slug: 'slick-portfolio-svelte',
-		color: '#ff3e00',
+		slug: 'worklin',
+		color: '#55ee55',
 		description:
-			'A Vercel-like developer portfolio website template made with Typescript and SvelteKit.',
+			'An prototype attendance management system with location checking.',
 		shortDescription:
-			'A Vercel-like developer portfolio website template made with Typescript and SvelteKit.',
-		links: [{ to: 'https://github.com/RiadhAdrani/slick-portfolio-svelte', label: 'GitHub' }],
-		logo: Assets.Svelte,
-		name: 'Slick Portfolio',
+		'An prototype attendance management system with location checking.',
+		links: [{ to: 'https://vtcinternationale.pythonanywhere.com/', label: 'Visit Website' }],
+		logo: Assets.VTC,
+		name: 'Worklin',
 		period: {
-			from: new Date()
+			from: new Date(2023, 5), to: new Date(2023, 6)
 		},
-		skills: getSkills('svelte', 'ts', 'tailwind', 'sass'),
-		type: 'Website Template',
+		skills: getSkills('django', 'jquery', 'tailwind', 'html'),
+		type: 'Dynamic Website',
 		screenshots: [
 			{
 				label: 'screen 1',
@@ -60,7 +59,22 @@ const MY_PROJECTS: Array<Project> = [
 				src: 'https://images.unsplash.com/photo-1585079542156-2755d9c8a094?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHByb2dyYW1taW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60'
 			}
 		]
-	}
+	},
+	{
+		slug: 'book-stack',
+		color: '#C850C0',
+		description:
+			'Library management system with in Java .',
+		shortDescription:'Hybrid online learning platform for KET academy.',
+		links: [{ to: 'https://ketacademy.web.app', label: 'Visit Website' , newTab: true}],
+		logo: Assets.KetAcademy,
+		name: 'BookStack Library',
+		period: {
+			from: new Date(2023, 0), to: new Date(2023, 2)
+		},
+		skills: getSkills('java', 'javafx', 'css', 'mysql'),
+		type: 'Desktop Application'
+	},
 ];
 
 export default MY_PROJECTS;
