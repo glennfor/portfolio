326e3436c9e5776034b32e216fa2a27099694eb6
Glen Nfor <glennfor72@gmail.com>
2023-12-31 11:49:38 -0500

    refactor: rebuild resume page as blog

---
+++ src/routes/writing/+page.svelte
@@ -0,0 +1,31 @@
+<script>
+	import Chip from '$lib/components/Chip/Chip.svelte';
+	import CommonPage from '$lib/components/CommonPage.svelte';
+	import { WRITING } from '$lib/params';
+
+	const { item, title } = WRITING;
+</script>
+
+<CommonPage {title}>
+	<div class="writing">
+		{#if item}
+			<a href={item}>
+				<Chip size={'1.25em'}>Download</Chip>
+			</a>
+		{:else}
+			<Chip>Ooops! no CV at the moment.</Chip>
+		{/if}
+	</div>
+</CommonPage>
+
+<style lang="scss">
+	.writing {
+		display: flex;
+		justify-content: center;
+		margin-top: 20px;
+
+		& > a {
+			color: inherit;
+		}
+	}
+</style>
